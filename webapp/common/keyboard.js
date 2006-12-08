2b07e3d47dada4f375873d1ff5f7c3683a05b121
spiegel <spiegel>
2006-12-08 15:33:52 +0000

    Simplified.  No more global key handlers, except for blocking F1 and Shift-F10.  Individual input fields are now supposed to install their own handlers.

--- webapp/common/keyboard.js
+++ webapp/common/keyboard.js
@@ -3,62 +3,59 @@
 
   enter = false;
 
-  if (isIE()) {
-    document.onkeydown = handleIEKeyDownEvent;
-    document.onhelp = handleIEKeyHelpEvent;
+  if (document.all) { 
+    // block help function in IE so we can use F1
+    document.onhelp = cancelKeyEvent;
   } else {
-    window.onkeydown = handleKeyDownEvent;
-    window.onkeypress = handleKeyPressEvent;
-    window.onhelp = handleKeyHelpEvent;
-    window.oncontextmenu = handleOnContextMenu;
-  }
-
-  function handleIEKeyDownEvent() {
-    return handleKeyDownEvent(event);
-  }
-
-  function handleIEKeyHelpEvent() {
-    return handleKeyHelpEvent(event);
+    // block context menu in FF so we can use Shift + F10
+    window.oncontextmenu = cancelKeyEvent;
   }
 
   function handleKeyDownEvent(eventObj) {
-    var keyCode = eventObj.keyCode;
-    if (keyCode == 13) {
-      cancelKeyEvent(eventObj);
-      sendFormWithKey("enter");
-    } else if ((!(eventObj.altKey)) 
-            && (!(eventObj.ctrlKey))
-            && (keyCode >=112)
-            && (keyCode <= 123)) {
-      cancelKeyEvent(eventObj);
-      handleFunctionKeyEvent(eventObj, keyCode);
+    eventObj = (eventObj) ? eventObj
+                          : ((window.event) ? window.event : "");
+    if (eventObj) {
+      var keyCode = eventObj.keyCode;
+      if (keyCode == 13) {
+        cancelKeyEvent(eventObj);
+        sendFormWithKey("enter");
+      } else if ((!(eventObj.altKey)) 
+              && (!(eventObj.ctrlKey))
+              && (keyCode >=112)
+              && (keyCode <= 123)) {
+        cancelKeyEvent(eventObj);
+        handleFunctionKeyEvent(eventObj, keyCode);
+      }
     }
   }
 
   function handleKeyPressEvent(eventObj) {
-    var iKC = eventObj.keyCode;
-    if ((!(eventObj.altKey)) && (!(eventObj.ctrlKey)) && (iKC >=112) && (iKC <= 123)) {
-      cancelKeyEvent(eventObj);
+    eventObj = (eventObj) ? eventObj
+                          : ((window.event) ? window.event : "");
+    if (eventObj) {
+      var keyCode = eventObj.keyCode;
+      if ((!(eventObj.altKey)) && 
+          (!(eventObj.ctrlKey)) && 
+          (keyCode >=112) && 
+          (keyCide <= 123)) {
+        cancelKeyEvent(eventObj);
+      }
     }
-  }
-
-  function handleKeyHelpEvent(eventObj) {
-    cancelKeyEvent(eventObj);
-  }
-
-  function handleOnContextMenu(eventObj) {
-    // Disable context menu to allow Shift + F10 
-    return false;
   }
 
   function cancelKeyEvent(eventObj) {
-    try {
-      eventObj.keyCode = 0;
-    } catch(e) {
-      eventObj.preventDefault();
+    eventObj = (eventObj) ? eventObj
+                          : ((event) ? event : "");
+    if (eventObj) {
+      try {
+        eventObj.keyCode = 0;
+      } catch(e) {
+        eventObj.preventDefault();
+      }
+      eventObj.cancelBubble = true;
+      eventObj.returnValue = false;
     }
-    eventObj.cancelBubble = true;
-    eventObj.returnValue = false;
+    return false;
   }
 
   function sendFormWithKey(strKey) {
@@ -85,10 +82,3 @@
     }
   }
 
-  function isIE() {
-    if (document.all) {
-      return true;
-    } else {
-      return false;
-    }
-  }

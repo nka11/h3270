2199aba35ed7335c5862fc1ee9031a0a2255c14d
spiegel <spiegel>
2007-02-19 15:40:50 +0000

    Initial revision.

---
+++ src/org/h3270/host/HostUnreachableException.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2003-2007 akquinet framework solutions
+ *
+ * This file is part of h3270.
+ *
+ * h3270 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * h3270 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with h3270; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, 
+ * MA 02110-1301 USA
+ */
+
+package org.h3270.host;
+
+/**
+ * Indicates that the host could not be reached by the s3270 process.
+ * This is most likely due to firewall problems.
+ * 
+ * @author Andre Spiegel spiegel@gnu.org
+ * @version $Id$
+ */
+public class HostUnreachableException extends S3270Exception {
+
+  public HostUnreachableException (String host, String reason) {
+    super ("Host " + host + " cannot be reached: " + reason);
+  }
+  
+}

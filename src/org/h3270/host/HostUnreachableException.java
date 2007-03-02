76a6bc3460928453f7c2641602f44205af8c5c2d
spiegel <spiegel>
2007-03-02 09:35:23 +0000

    (host, reason): New fields and accessor methods.

--- src/org/h3270/host/HostUnreachableException.java
+++ src/org/h3270/host/HostUnreachableException.java
@@ -30,8 +30,28 @@ package org.h3270.host;
  */
 public class HostUnreachableException extends S3270Exception {
 
+  private String host;
+  private String reason;
+  
   public HostUnreachableException (String host, String reason) {
     super ("Host " + host + " cannot be reached: " + reason);
+    this.host = host;
+    this.reason = reason;
+  }
+
+  /**
+   * Returns the name of the host that could not be reached.
+   */
+  public String getHost() {
+    return host;
+  }
+
+  /**
+   * Returns the reason why the host could not be reached, as reported
+   * by s3270.
+   */
+  public String getReason() {
+    return reason;
   }
   
 }

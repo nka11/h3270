b231f3b54098c6e58a17f38f7a7c49f0912cf2d0
spiegel <spiegel>
2007-03-02 09:47:08 +0000

    Initial revision.

---
+++ test/src/org/h3270/test/S3270Test.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2003-2007 akquinet framework solutions
+ *
+ * This file is part of h3270.
+ *
+ * h3270 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * h3270 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with h3270; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+package org.h3270.test;
+
+import junit.framework.*;
+
+import org.h3270.host.*;
+import org.h3270.render.*;
+
+/**
+ * Tests the basic interface to s3270 (starting the process, catching errors).
+ * 
+ * @author Andre Spiegel spiegel@gnu.org
+ * @version $Id$
+ */
+public class S3270Test extends TestCase {
+
+  public S3270Test (String name) {
+    super (name);
+  }
+  
+  public void testUnknownHost() {
+    H3270Configuration config =
+      H3270Configuration.create ("webapp/WEB-INF/h3270-config.xml");
+    try {
+      new S3270 ("cunvm.cuny.ed", config);
+      fail ("should have thrown UnknownHostException");
+    } catch (UnknownHostException ex) {
+      assertEquals("cunvm.cuny.ed", ex.getHost());
+    }
+  }
+}
